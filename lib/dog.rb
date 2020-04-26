88997fc23045ec72614fd3f3c4e72ee601ca2493
Koray Ozkal <koryozkal@gmail.com>
2020-04-26 03:23:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,17 +10,20 @@ class Dog
     @@all << self
   end
 
+  def save
+    @@all << self
+  end
+
   def self.clear_all
-		@@all.clear
+    @@all.clear
+  end
 
-	end
+  def self.print_all
+    @@all.each { |dog| puts dog.name}
+    end
+  end 
+end 
 
-	def self.all
-		@@all.each do |dog|
-			puts dog.name
-		end
-	end
-end
-  
+
   
  
